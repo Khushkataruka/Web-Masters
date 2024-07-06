c65cd3ce5bae12b3568294c247efd4b5982c3c6b
Khush <04khushkat@gmail.com>
2024-07-06 18:32:05 +0530

    Planet

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import Background from "./Components/Home page/Background"
+import Planets from "./Components/Planets/Planets"
+import Navbar from "./Components/Home page/Navbar"
 function App() {
 
 
   return (
     <>
-      <Background/>
+    {/* <div className="Container">
+    <Navbar/>
+      <div className="images">
+      </div>
+    </div> */}
+    <Planets/>
     </>
   )
 }
 
 export default App
+
