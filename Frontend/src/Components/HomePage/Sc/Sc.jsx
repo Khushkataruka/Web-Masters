81c54ab11bf4606b859c14a24abc3cd3387ad772
loke-19181 <makamlokesh.ai@gmail.com>
2024-08-08 00:11:56 +0530

    changes

--- Frontend/src/Components/HomePage/Sc/Sc.jsx
+++ Frontend/src/Components/HomePage/Sc/Sc.jsx
@@ -18,7 +18,7 @@ const Sc = () => {
     return (
         <>
             <div className="space-center">
-                <div className='box' data-aos='fade-down'>
+                <div className='box' data-aos='fade-up'>
                     <div className="text"><h2>Space Centers</h2></div>
                     <div className="b1"> <Link to="/center" className="link">Learn more</Link></div>
                 </div>
